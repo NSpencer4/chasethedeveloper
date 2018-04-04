b8318d793ce3b3d242b43b13b82cac8a044912c8
nspencer4 <nicholas.spencer@scotts.com>
2018-04-03 21:45:42 -0400

    Finished timeline (unresponsive currently)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,105 @@ import { ChartModule } from 'primeng/components/chart/chart';
 import { Chart } from 'chart.js';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  data: any;
+  data1: any;
+  data2: any;
+  data3: any;
+  data4: any;
+  data5: any;
+  data6: any;
   options: any;
 
   public constructor() {
-    this.data = {
-        datasets: [
-            {
-                data: [80, 20],
-                backgroundColor: [
-                    '#3ebdf0',
-                    '#cbcacb'
-                ],
-                hoverBackgroundColor: [
-                    '#3ebdf0',
-                    '#cbcacb'
-                ]
-            }]
-        };
+    this.data1 = {
+      datasets: [
+        {
+          data: [100, 0],
+          backgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ],
+          hoverBackgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ]
+        }]
+    };
+    this.data2 = {
+      datasets: [
+        {
+          data: [100, 0],
+          backgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ],
+          hoverBackgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ]
+        }]
+    };
+    this.data3 = {
+      datasets: [
+        {
+          data: [100, 0],
+          backgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ],
+          hoverBackgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ]
+        }]
+    };
+    this.data4 = {
+      datasets: [
+        {
+          data: [100, 0],
+          backgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ],
+          hoverBackgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ]
+        }]
+    };
+    this.data5 = {
+      datasets: [
+        {
+          data: [90, 10],
+          backgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ],
+          hoverBackgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ]
+        }]
+    };
+    this.data6 = {
+      datasets: [
+        {
+          data: [90, 10],
+          backgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ],
+          hoverBackgroundColor: [
+            '#3ebdf0',
+            '#cbcacb'
+          ]
+        }]
+    };
 
     this.options = {
       cutoutPercentage: 80
